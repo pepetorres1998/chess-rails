3a5cb8360276bd3d4d56f29191b99b540172915a
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2023-12-19 10:28:40 -0600

    Print chessboard in view

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
+import { Chessboard, FEN, INPUT_EVENT_TYPE } from "cm-chessboard"
+// import { Markers } from "cm-chessboard-extensions/" // this needs to be pinned, the cm-chessboard project has the sources for the extensions
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    //this.element.textContent = "Hello World!"
+    const board = new Chessboard(document.getElementById("board"), {
+      position: FEN.start,
+      responsive: true,
+      assetsUrl: "./assets/"
+    })
+
+    board.enableMoveInput(this.inputHandler)
+  }
+
+  inputHandler(event) {
+    console.log(event)
+    if (event.type === INPUT_EVENT_TYPE.moveInputStarted ||
+      event.type === INPUT_EVENT_TYPE.validateMoveInput) {
+      return true // false cancels move
+    }
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "chess.js", to: "https://ga.jspm.io/npm:chess.js@1.0.0-beta.6/dist/esm/chess.js"
+pin "cm-chessboard", to: "https://ga.jspm.io/npm:cm-chessboard@8.5.0/src/Chessboard.js"
