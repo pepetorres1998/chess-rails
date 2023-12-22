6bc6480430e3785ee64609898d442a3ba7ba7bd2
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2023-12-21 20:15:43 -0600

    Add chess logic into chessboard

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,26 +1,55 @@
 import { Controller } from "@hotwired/stimulus"
 import { Chessboard, FEN, INPUT_EVENT_TYPE } from "cm-chessboard"
 import { MARKER_TYPE, Markers } from "cm-chessboard-markers"
-// import { Markers } from "cm-chessboard-extensions/" // this needs to be pinned, the cm-chessboard project has the sources for the extensions
+import { Chess } from "chess.js"
+
+const chess = new Chess()
+
+const board = new Chessboard(document.getElementById("board"), {
+  position: FEN.start,
+  responsive: true,
+  assetsUrl: "./assets/",
+  extensions: [{ class: Markers }]
+})
 
 export default class extends Controller {
   connect() {
     //this.element.textContent = "Hello World!"
-    const board = new Chessboard(document.getElementById("board"), {
-      position: FEN.start,
-      responsive: true,
-      assetsUrl: "./assets/",
-      extensions: [{ class: Markers }]
-    })
-
     board.enableMoveInput(this.inputHandler)
   }
 
   inputHandler(event) {
-    console.log(event)
-    if (event.type === INPUT_EVENT_TYPE.moveInputStarted ||
-      event.type === INPUT_EVENT_TYPE.validateMoveInput) {
-      return true // false cancels move
+    console.log("inputHandler", event)
+    if (event.type === INPUT_EVENT_TYPE.movingOverSquare) {
+      return // ignore this event
+    }
+    if (event.type !== INPUT_EVENT_TYPE.moveInputFinished) {
+      event.chessboard.removeMarkers(MARKER_TYPE.dot)
+      event.chessboard.removeMarkers(MARKER_TYPE.bevel)
+    }
+    if (event.type === INPUT_EVENT_TYPE.moveInputStarted) {
+      const moves = chess.moves({ square: event.squareFrom, verbose: true })
+      for (const move of moves) { // draw dots on possible squares
+        if (move.promotion && move.promotion !== "q") {
+          continue
+        }
+        if (event.chessboard.getPiece(move.to)) {
+          event.chessboard.addMarker(MARKER_TYPE.bevel, move.to)
+        } else {
+          event.chessboard.addMarker(MARKER_TYPE.dot, move.to)
+        }
+      }
+      return moves.length > 0
+    } else if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
+      const move = { from: event.squareFrom, to: event.squareTo, promotion: event.promotion }
+      const result = chess.move(move, { sloppy: true })
+      if (result) {
+        event.chessboard.state.moveInputProcess.then(() => { // wait for the move input process has finished
+          event.chessboard.setPosition(chess.fen(), true).then(() => { // update position, maybe castled and wait for animation has finished
+          })
+        })
+      }
+      return result
     }
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "chess.js", to: "https://ga.jspm.io/npm:chess.js@1.0.0-beta.6/dist/esm/chess.js"
+pin "chess.js", to: "https://cdn.jsdelivr.net/npm/chess.mjs@1/src/chess.mjs/Chess.js"
 pin "cm-chessboard", to: "https://ga.jspm.io/npm:cm-chessboard@8.5.0/src/Chessboard.js"
 pin "cm-chessboard-markers", to: "https://ga.jspm.io/npm:cm-chessboard@8.5.0/src/extensions/markers/Markers.js"
