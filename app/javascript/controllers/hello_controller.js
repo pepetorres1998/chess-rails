63ec0abaa61909dfb357317ae72efb1c688359e2
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2023-12-22 09:01:45 -0600

    Add promotion dialog and checkmate logic

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import { Chessboard, FEN, INPUT_EVENT_TYPE } from "cm-chessboard"
 import { MARKER_TYPE, Markers } from "cm-chessboard-markers"
+import { PROMOTION_DIALOG_RESULT_TYPE, PromotionDialog } from "cm-chessboard-promotion-dialog"
 import { Chess } from "chess.js"
 
 const chess = new Chess()
@@ -9,7 +10,10 @@ const board = new Chessboard(document.getElementById("board"), {
   position: FEN.start,
   responsive: true,
   assetsUrl: "./assets/",
-  extensions: [{ class: Markers }]
+  extensions: [
+    { class: Markers },
+    { class: PromotionDialog }
+  ]
 })
 
 export default class extends Controller {
@@ -39,7 +43,7 @@ export default class extends Controller {
           event.chessboard.addMarker(MARKER_TYPE.dot, move.to)
         }
       }
-      return moves.length > 0
+      return true
     } else if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
       const move = { from: event.squareFrom, to: event.squareTo, promotion: event.promotion }
       const result = chess.move(move, { sloppy: true })
@@ -48,8 +52,41 @@ export default class extends Controller {
           event.chessboard.setPosition(chess.fen(), true).then(() => { // update position, maybe castled and wait for animation has finished
           })
         })
+      } else {
+        // promotion?
+        let possibleMoves = chess.moves({ square: event.squareFrom, verbose: true })
+        for (const possibleMove of possibleMoves) {
+          if (possibleMove.promotion && possibleMove.to === event.squareTo) {
+            event.chessboard.showPromotionDialog(event.squareTo, chess.turn(), (result) => {
+              console.log("promotion result", result)
+              if (result.type === PROMOTION_DIALOG_RESULT_TYPE.pieceSelected) {
+                chess.move({ from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1) })
+                event.chessboard.setPosition(chess.fen(), true)
+              } else {
+                // promotion canceled
+                console.log("promotion canceled")
+                // event.chessboard.enableMoveInput(inputHandler, COLOR.white)
+                event.chessboard.setPosition(chess.fen(), true)
+              }
+            })
+            return true
+          }
+        }
       }
       return result
+    } else if (event.type === INPUT_EVENT_TYPE.moveInputFinished) {
+      if (chess.game_over()) {
+        let gameWinner;
+
+        if (chess.in_checkmate()) {
+          gameWinner = chess.turn() == "w" ? "Black" : "White"
+        } else {
+          gameWinner = "Nobody"
+        }
+        alert(`${gameWinner} wins!`)
+        chess.reset()
+        event.chessboard.setPosition(chess.fen(), true)
+      }
     }
   }
 }
